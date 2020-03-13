d99c82cc0f8eca9a6fbeeb4f01ffd8f0729fdaae
Roshan Kumar <roshankunaguero09@gmail.com>
2020-03-13 15:11:44 +0530

    first commit

---
+++ precache-manifest.fc586f4eaf30645f81bb675117a776ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17e356d3a1cb1d1387e9d6f471d7fb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "024ac94a2729c87d64ec",
+    "url": "/static/css/main.c36bc3aa.chunk.css"
+  },
+  {
+    "revision": "08f678b474807c373c6a",
+    "url": "/static/js/2.8e84aa65.chunk.js"
+  },
+  {
+    "revision": "a0a3063ed0e7e344881a79d4bea9f313",
+    "url": "/static/js/2.8e84aa65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024ac94a2729c87d64ec",
+    "url": "/static/js/main.06f1ff01.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
